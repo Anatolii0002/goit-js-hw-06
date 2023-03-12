e52bc17cfefb9b65f7fe1b5c10ce735fa3d58d64
Анатолій <111740176+Anatolii0002@users.noreply.github.com>
2023-03-12 15:53:57 +0200

    зміни

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,8 @@
 const categoriesList = document.querySelector("#categories");
 const categoriesItems = categoriesList.querySelectorAll(".item");
 
-// Виведення кількості категорій
 console.log(В сспику ${categoriesItems.length} категорії.);
 
-// Виведення тексту заголовку та кількості елементів у кожній категорії
 categoriesItems.forEach(item => {
 const heading = item.querySelector("h2").textContent;
 const itemsLength = item.querySelectorAll("li").length;
